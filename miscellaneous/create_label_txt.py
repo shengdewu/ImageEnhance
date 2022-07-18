7460a2d0f72c74932d8ba810cf6033d0a7c65d93
boofish <boofish@xintu.com>
2022-07-18 17:24:00 +0800

    update

--- miscellaneous/create_label_txt.py
+++ miscellaneous/create_label_txt.py
@@ -128,9 +128,11 @@ def _label(root_path, pair_name, flag='', test_ratio=0.05):
                     tl.write('{},{}\n'.format(name[1], name[0]))
 
 
-def _search_rt(rt_name):
+def _search_rt(rt_name, proportion=1.):
     with open(os.path.join(rt_name), mode='r') as igt:
         rt_inputs = [r.strip('\n') for r in igt.readlines()]
+    if 1. > proportion > 0.:
+        rt_inputs = random.sample(rt_inputs, int(proportion * len(rt_inputs)))
     return rt_inputs
 
 
@@ -168,9 +170,9 @@ def create_train_label(root_path):
     _label(root_path, select_name, 'no_aug')
 
 
-def _create_dup_dict(path, name_key=None):
+def _create_dup_dict(path, name_key=None, proportion=1.):
     name_dict = dict()
-    for name in _search_rt(os.path.join(path)):
+    for name in _search_rt(os.path.join(path), proportion=proportion):
         if name_key is not None and name.split('/')[1].find(name_key) == -1:
             continue
         key = '{}.{}'.format(name.split('/')[1].split('g')[0], name.split('/')[1].split('.')[-1])
@@ -181,23 +183,19 @@ def _create_dup_dict(path, name_key=None):
 
 
 def create_train_no_aug_label(root_path, max_rts=-1):
-    gts = _search_rt(os.path.join(root_path, 'gt_8bit_1500p.txt'))
+    gts = _search_rt(os.path.join(root_path, 'gt_16bit_540p_only_adjust_light.txt'))
     rt_input_dict_list = list()
 
     if max_rts != 0:
-        rts = _search_rt(os.path.join(root_path, 'rt_8bit_1500p.txt'))
+        rts = _search_rt(os.path.join(root_path, 'rt_tif_16bit_540p.txt'))
         if max_rts > 1:
             rts = random.sample(rts, min(max_rts, len(rts)))
         elif max_rts > 0:  # [0.0, 1)
             rts = random.sample(rts, int(max_rts*len(rts)))
         rt_input_dict_list.append(dict([(name.split('/')[1], name) for name in rts]))
 
-    # rt_input_dict_list.append(_create_dup_dict(os.path.join(root_path, 'rt_tif_16bit_540p_overexposure.only.light.v2_01.txt')))
-    # rt_input_dict_list.append(_create_dup_dict(os.path.join(root_path, 'rt_tif_16bit_540p_underexposure.only.light.v2_01.txt')))
-    # rt_input_dict_list.append(_create_dup_dict(os.path.join(root_path, 'rt_tif_16bit_540p_overexposure.only.light.light.light_01.txt')))
-    # rt_input_dict_list.append(_create_dup_dict(os.path.join(root_path, 'rt_tif_16bit_540p_underexposure.only.light.dark.dark_01.txt')))
-    # rt_input_dict_list.append(_create_dup_dict(os.path.join(root_path, 'rt_tif_16bit_540p_underexposure.only.light.light.light_01.txt')))
-    # rt_input_dict_list.append(_create_dup_dict(os.path.join(root_path, 'rt_tif_16bit_540p.only.light.local.light.v2_01.txt')))
+    rt_input_dict_list.append(_create_dup_dict(os.path.join(root_path, 'rt_tif_16bit_540p_overexposure.only.light.v3_01.txt'), proportion=0.2))
+    rt_input_dict_list.append(_create_dup_dict(os.path.join(root_path, 'rt_tif_16bit_540p.only.light.local.light.v3_01.txt')))
 
     train_pair = list()
     for ii in gts:
@@ -213,8 +211,8 @@ def create_train_no_aug_label(root_path, max_rts=-1):
                 else:
                     train_pair.append((ii, rt_input_dict[k]))
 
-    _label(root_path, train_pair, 'all_jpg_test')
+    _label(root_path, train_pair, 'only_adjust_light_base.ov.loc')
 
 
 if __name__ == '__main__':
-    create_train_no_aug_label('/mnt/sdb/data.set/xintu.data/enhance.data/xt.image.enhancement.540.jpg', 1000)
+    create_train_no_aug_label('/mnt/sdb/data.set/xintu.data/enhance.data/xt.image.enhancement.540', -1)
