34d12bca7752545fcbf30229cb0dc6bc13edbabf
baoyong <you@example.com>
2022-08-05 14:35:25 +0800

    add lapyrid no gan

--- codes/model/__init__.py
+++ codes/model/__init__.py
@@ -3,3 +3,4 @@ from codes.model.image_enhance_by_dce import DceModel
 from codes.model.image_enhance_by_curl import CurlModel
 from codes.model.image_enhance_by_lap import LapGanModel
 from codes.model.image_enhance_by_lut import LutModel
+from codes.model.image_enhance_by_lap_cls import LapModel
